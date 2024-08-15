6426b9f5e49bc233ea88e67d13396e8f8ae8b4b5
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-15 13:31:20 -0700

    Trying to get page to render

--- src/index.jsx
+++ src/index.jsx
@@ -8,3 +8,34 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+import React from 'react'; 
+import ReactDOM from 'react-dom/client';
+
+
+// Defiing the app
+function App(){
+    <div>
+        <h1>Setting Up a Project for Development</h1>
+        <ul>
+            <p>You want to navigate into the folder that you are going to be workig in through the terminal</p>
+            <li> cd "project folder" </li>
+            <p>You want to create a .gitignore and then copy the standard things to ignore from github/node page. </p>
+            <li>touch .gitignore</li>
+            <p>Now you want to initialize git</p>
+            <li>git init</li>
+            <p>add all files to working tree</p>
+            <li>git add .</li>
+            <p>submit your initial commit</p>
+            <li>git commit -m "initial commit"</li>
+        </ul>
+    </div>
+}
+
+// Rendering the app
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  
